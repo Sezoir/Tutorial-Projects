7e78b2905591d2d445989e98c0cc3e2ba2a4cd35
Josh Mottley <josh.mottley@ntlworld.com>
2017-10-02 14:48:10 +0100

    - Added Inheritance and Polymorphism Definitions.

--- Training/Definitions.cpp
+++ Training/Definitions.cpp
@@ -81,7 +81,7 @@ Class Inheritance:	- Class inheritance is the ability to reuse code that is deri
 					  base class.
 
 ------------------------------------------------------------------------------------------------------
-Principles
+Principles for Obect Orientation Programming
 ------------------------------------------------------------------------------------------------------
 Name:		- What is it? i.e Event, Document Employee
 			- Sometimes referred as the "type".
@@ -104,8 +104,47 @@ Abstraction:	- Abstraction means that we focus on the essential qualities of som
 				- For example, if we have to different bank accounts, the essentials(abstraction) for any bank account would be
 				  the bank account numbers, the balance.
 
+Encapsulation:	- This first refers to the ideas of taking your attributes and then your behaviors and "bundling" them together in the 
+				  the same unit, or class.
+				- However this is expanded as you also want to restrict access to the inner workings of that class, or any objects
+				  based on that class. This is refered to as location hiding, or data hiding.
+				- The principle is that a object should not reveal anything about itself except what is absolutely neccessary for other
+				  parts of the project to work.
+				- For example, with a bank account, you do not want anything else to reach in and change the balance of the account 
+				  without going throught the deposit or withdraw behaviour, so we can hide that attribute and control access to it
+				  with the behaviour. So it can only be changed from within the class and not directly changed from outside of the
+				  object.
+				- This is also refered as "black boxing" as we are closing off more and more of the inner working of the object except
+				  for the few pieces that we decide to make public for say input and output.
+				- One of the benefits of object-oreintation is that it allows us to change the working of the object as we have 
+				  hidden the data. For example we could change the working of a class without breaking the other parts of the 
+				  application.
+				- This is less about being secretive but more about reducing the dependency between different parts of the application,
+				  so that one change doesnt spiral down the path causing for much more changes.
+				- The general rule for how much should you hide, is that you should try and hide as much as possible.
+				
+Inheritance:	- The first idea of inheritance is the idea of code reuse.
+				- We can create a new class, but instead of writing it from class, we can use an existing class.
+				- For example if we have a person class, and we want to add a new customer class, however this class has all the 
+				  behaviour and attributes of the person class, we don't want to add the new attributes/behaviour to the person
+				  class, as a person may not be a customer.
+				  We could create a new class and copy the presons attributes/behaviour however it is a better idea to instead create
+				  a class called customer and inherit from the person class. This means our new customer class automatically has
+				  everything the person class has without writing any code. In this case the person would be called the superclass/
+				  parent class and the customer class would be called the subclass/child class.
+				- You can have more than one classes inheriting from one superclass.
+				- The advantage is, if you make a change to the superclass, it will automatically filter down to the subclasses, so no
+				  further changes must be made.
 
-
-
+Polymorphism:	- This lets you automatically do the create behaviour even if what we are working with take one or many forms.
+				- For example the plus sign (+), if we use the + for 2 integer it will add the integers together, however if we use the
+				  plus sign for strings it will combines the strings together, so it chooses the correct behaviour for the job.
+				- Another example for application, is if we have a superclass bank account class, then we have several class that 
+				  inherit from the bank account, called: Checking Account, Saving Account and Investment Account. The Investment
+				  Account may have a attribute which issues a penalty for taking money out with 30 days notifiction, so you can overide
+				  the method of the inherited superclass.
+				- Polymorphism lets us work freely with objects that have been created from any of these classes. So we could call the
+				  withdraw method without knowing which class it was instantiated from due to the behaviour of each one.
+				- Polymorphism is often only need when it is needed for a class and is not often used.
 
 */
