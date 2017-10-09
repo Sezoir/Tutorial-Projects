c3007bba642bb21bde7b3d6ca6950871ecfba5c9
Josh Mottley <josh.mottley@ntlworld.com>
2017-10-09 22:12:32 +0100

    - Fixed mismatch with files.

--- Training/Structured IO.cpp
+++ Training/Structured IO.cpp
@@ -1,32 +1,48 @@
 #define _CRT_SECURE_NO_DEPRECATE
-#include <cstdio>
-using namespace std;
+#include <stdio.h>
+#include <string.h>	// Used for "strncpy".
+#include <stdint.h>	// Used for "uint8_t" type.
 
-int main()
+enum {slen = 128};	// Defines a constant.
+
+struct s1
 {
-	const char * fn = "test.file";
-	const static int maxstring = 1024;
-	const char * str = "This is a literal C-String.\n";
+	uint8_t a;	// Used for when you want a byte that is being used as a integer.
+	uint8_t b;
+	char s[slen];
+};
 
-	// Create/write the file
+void main()
+{
+	const char *fn = "test2.bin";	// Use ".bin" to create a binary file for the write/read binary mode.
+	const char * str = "This is a literal C-String\n";
+
+	// Create/Read the file
 	printf("Writing file\n");
-	FILE * fw = fopen(fn, "w");	// fopen takes 2 arguments; the file name, and then the mode string.
-	for (int i = 0; i < 5; i++)	// Write the string out 5 times.
+	FILE * fw = fopen(fn, "wb");	// "wb" means write binary mode.
+
+	struct s1 buf1;
+	for (int i = 0; i < 5; i++)	// Used to populate the buf1
 	{
-		fputs(str, fw);	// fputs is a line orientated write, so writes a line at a time to the file.
+		buf1.a = i;
+		buf1.b = strlen(str);
+		strncpy(buf1.s, str, slen);	// Used incase the string is too long for the buffer, you can stop it and now overwrite your string.
+		fwrite(&buf1, sizeof(struct s1), 1, fw);	// "fwrite" is the binary equivalent of the "fputs". 
 	}
-	fclose(fw);	// Close the file
+
+	fclose(fw);
 	printf("Done.\n");
 
-	// Read File
-	printf("Reading File\n");
-	char buf[maxstring];
-	FILE * fr = fopen(fn, "r");	// "r" for read.
-	char * rc;
-	while ((rc = fgets(buf, maxstring, fr))) // "fgets" reads the file, the arguments are: stored, buffer, file handle.
-	{										 // this returns a pointer to the buffer or null for the end of the file.
-		printf(buf);
+	// Read the file
+	printf("Reading file\n");
+	FILE * fr = fopen(fn, "rb");
+	struct s1 buf2;
+	int rc;
+	while ((rc = fread(&buf2, sizeof(struct s1), 1, fr) ))	// "fread" is the binary equivalent of the "fgets"
+	{
+		printf("a: %d, b: %d, s: %s", buf2.a, buf2.b, buf2.s);
 	}
+
 	fclose(fr);
 	printf("Done.\n");
 
@@ -34,6 +50,8 @@ int main()
 }
 
 /*
-	- "fopen" returns a "file handle". This is a pointer to a file structure that is defined in stdio.h.
-	- "FILE" is a structure type.
+	- Note that: 
+		- "strncpy" 3 arguments are: the destination of the string, string itself, maximum lenth of the buffer.
+		- "fwrite" 4 arguments are: pointer to the buf, size of the buffer, number of the buffers, file handle.
+		- "fread" 4 arguments are: pointer to the buf, size of the buffer, number of the buffers, file handle.
 */
